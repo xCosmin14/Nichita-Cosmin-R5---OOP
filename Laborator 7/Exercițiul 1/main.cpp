1abdc2321926824e003cfbe69c2ca45c1774cad7
Cosmin Nichita <95240438+xCosmin14@users.noreply.github.com>
2026-04-21 20:15:18 +0300

    Create main.cpp

---
+++ Laborator 7/Exercițiul 1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ex1_temperaturi.h"
+using namespace std;
+ 
+int main() {
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
+ 
+    cout << "300 Kelvin      = " << a << " grade Celsius\n";
+    cout << "120 Fahrenheit  = " << b << " grade Celsius\n";
+  
+    return 0;
+}
+ 
